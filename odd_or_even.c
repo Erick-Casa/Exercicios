e6e14bfbf888826ba01027466cbbb9c9cd069159
Erick de Souza Casa <ericksouza1777@gmail.com>
2025-02-18 16:17:06 -0400

    Add files via upload

---
+++ odd_or_even.c
@@ -0,0 +1,17 @@
+//user's number input, process and print the result odd or even
+#include <stdio.h>
+#define true 1
+
+int number = 0;
+int main(){
+	while(true){
+	printf("Odd or Even verificator - Input the number: ");
+	scanf("%i", &number);
+	if(number % 2 == 0){
+		printf("The number is Even\n");
+	}else{
+		printf("The number is Odd\n"); 
+		 }
+		  
+			   }
+		  }
