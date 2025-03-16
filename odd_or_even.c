e2050a4e1c13651217dc15c1d148fa9055e13ea9
Erick de Souza Casa <ericksouza1777@gmail.com>
2025-03-16 08:29:32 -0400

    Update old exercise

--- odd_or_even.c
+++ odd_or_even.c
@@ -1,17 +1,43 @@
-//user's number input, process and print the result odd or even
-#include <stdio.h>
-#define true 1
+//read user's number input, process and print the result odd or even
 
-int number = 0;
-int main(){
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void read_int(int *ptr_number){
 	while(true){
-	printf("Odd or Even verificator - Input the number: ");
-	scanf("%i", &number);
-	if(number % 2 == 0){
-		printf("The number is Even\n");
-	}else{
-		printf("The number is Odd\n"); 
-		 }
-		  
-			   }
-		  }
+	printf("Write the number to check if it is odd or even, type 0 to exit: ");
+	
+	if(scanf("%d", ptr_number) != 1 ){    //treatment for invalid characters
+		getchar();
+		printf("Write an integer!\n");
+		continue;
+	}
+		
+	if(*ptr_number == 0){
+		exit(EXIT_SUCCESS);
+	}
+	
+	break;
+	}
+}
+
+void print_parity(int number){
+	if(number % 2 == 0){	//if number divided by 2 equals 0 is even else odd
+		printf("The number is even\n");
+	}
+	else{
+		printf("The number is odd\n"); 
+	}		
+}
+
+int main(){
+	int number = 0;
+	
+	while(true){
+		read_int(&number);
+		print_parity(number);		  
+	}
+	
+	return 0;
+}
